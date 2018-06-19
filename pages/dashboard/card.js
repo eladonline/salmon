bc3c1985f016eeefb90790fba0fb508423d90d81
danieliraz <dan.eliraz@gmail.com>
2018-06-19 15:20:11 +0300

    hocs shortcut

--- pages/dashboard/card.js
+++ pages/dashboard/card.js
@@ -1,7 +1,6 @@
 import Helmet from 'react-helmet';
-import Page from '../../hocs/privatePage';
-;
-import Card from '../../containers/Ecommerce/card';
+import Page from 'hocs/privatePage';
+import Card from 'themContainers/Ecommerce/card';
 
 export default Page(() => (
   <div>
